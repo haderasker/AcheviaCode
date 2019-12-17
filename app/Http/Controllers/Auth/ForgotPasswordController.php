23950910932abe1149c78e8b85249cf5d42ac1d8
Hadeer Asker <eng.hader2012@gmail.com>
2019-12-17 13:18:34 +0200

    forget password6

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -25,13 +25,13 @@ class ForgotPasswordController extends Controller
     /**
      * Send a reset link to the given user.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
     public function sendResetLinkEmail(Request $request)
     {
-        dd($request);
-        $this->validateEmail($request);
+         $this->validateEmail($request);
+
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
@@ -40,8 +40,9 @@ class ForgotPasswordController extends Controller
             $this->credentials($request)
         );
 
-        return $response == Password::RESET_LINK_SENT
+       $response == Password::RESET_LINK_SENT
             ? $this->sendResetLinkResponse($request, $response)
             : $this->sendResetLinkFailedResponse($request, $response);
+       dd($response);
     }
 }
